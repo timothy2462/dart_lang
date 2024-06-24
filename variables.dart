7c226d0285165694748d10dd14a3a99752d209ad
unknown <timothy.christian10@gmail.com>
2024-06-24 21:47:57 +0100

    variables in dart

---
+++ variables.dart
@@ -0,0 +1,21 @@
+void main() {
+    // variables 
+
+    //Strings
+    //var name = "Samson";
+
+   String occupation = "Mobile developer";
+    
+    //Integer 
+    int age = 25;
+
+    //dynamic
+    dynamic height = 3.5;
+
+    //const and final
+    const fullName = "Timothy Akobundu";
+
+    final int date_of_birth = 2000;
+
+    print(date_of_birth);
+}
