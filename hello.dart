8d82426abde60ce7faa93e430407bb438eaa7ccf
unknown <timothy.christian10@gmail.com>
2024-06-25 23:27:53 +0100

    print

--- hello.dart
+++ hello.dart
@@ -1,4 +1,6 @@
 void main() {
-    print("hello tim welcome to dart programing language dart is a cool language");
+    String new_language = "dart";
+    print(new_language);
+    // print("hello tim welcome to dart programing language dart is a cool language");
 }
 
