a6b91a7dc670918955550fc0fdbed0b9f93aac02
unknown <timothy.christian10@gmail.com>
2024-07-02 02:22:57 +0100

    playing around dart

--- hello.dart
+++ hello.dart
@@ -2,5 +2,18 @@ void main() {
     String new_language = "dart";
     print(new_language);
     // print("hello tim welcome to dart programing language dart is a cool language");
+    int age = 25;
+    print(age / 10);
+    print(age % 10);
+    print(age + 10);
+    print(age - 10);
+    // for( var i = 0; i > 3; i++ ) {
+    //     print(" age $i" );
+    // }
+    print("my programing language is $new_language and my age is $age");
 }
 
+// void main() {
+//     var name = "Timothy";
+//     print(name);
+// }
