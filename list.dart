411e71151fecb808e3c3fb92c9dc29c58d8e4c0f
unknown <timothy.christian10@gmail.com>
2024-07-02 22:31:34 +0100

    sets

--- list.dart
+++ list.dart
@@ -3,8 +3,13 @@ void main( ) {
   List <int> scores = [50, 30, 75, 39];
   print(scores[2]); 
   scores.add(23);
-  scores.removeLast();
-    scores.remove(50);
+ // scores.removeLast();
+    //scores.remove(50);
+    scores.shuffle();
 
   print(scores); 
+  //List allows duplicate of data while sets does not allow duplicates of data
+
+  Set <String>  names = {"mario", "daniel", "samuel"};
+  print(names);
 }
