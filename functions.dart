0f1949bb30067fd8b2b9e2a60fd17532ef2a06c7
unknown <timothy.christian10@gmail.com>
2024-07-02 03:21:04 +0100

    functions with named parameters

--- functions.dart
+++ functions.dart
@@ -1,6 +1,8 @@
 void main() {
  final greeting = greet("Timothy", 23);
- print(greeting);
+//  print(greeting);
+final job = occupation( role: " Engineer",  salary: 9000 );
+print(job);
 
 }
 
@@ -8,3 +10,6 @@ greet(String name, int age) {
     return "hiii my name is $name and i am $age years old";
 } 
 
+ String occupation ({String? role,  required  salary}) {
+    return "Hello world my name is Timothy and i'm a $role and i earn $salary";
+}
